255ec9ad97a5c205bd2f4d6da1276f291cdb4d99
hay-bams <purposenigeria@gmail.com>
2020-12-27 12:10:55 +0100

    write test for invalid course registration

--- server/src/__tests__/mutations/course/index.test.ts
+++ server/src/__tests__/mutations/course/index.test.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { COURSES_DATA, STUDENT_DATA } from '../../test_data';
+import { COURSES_DATA } from '../../test_data';
 import { createTestServer } from '../../helpers';
-import { TEST_REGISTER_COURSE, TEST_UNREGISTER_COURSE, TEST_ADD_NEW_STUDENT, TEST_ADD_NEW_COURSE } from '../mutations';
+import { TEST_ADD_NEW_COURSE } from '../mutations';
 
 describe('Course Mutation', () => {
-  test.only('should add a new course', async () => {
+  test('should add a new course', async () => {
     const { mutate } = createTestServer({
       db: {
         courses: {
@@ -28,6 +28,32 @@ describe('Course Mutation', () => {
     expect(res.data.addNewCourse).toHaveProperty('id');
     expect(res.data.addNewCourse).toHaveProperty('name');
     expect(res.data.addNewCourse).toHaveProperty('totalSeats');
+    expect(res).toMatchSnapshot();
+  })
+
+
+  test.only('should throw an error if course already exists', async () => {
+    const { mutate } = createTestServer({
+      db: {
+        courses: {
+          findOne: jest.fn(() => (COURSES_DATA[0])),
+        }
+      },
+    });
+
+    const res = await mutate({
+      mutation: TEST_ADD_NEW_COURSE,
+      variables: {
+        input:  {
+          name: COURSES_DATA[0].name,
+          totalSeats: COURSES_DATA[0].totalSeats,
+          status: COURSES_DATA[0].status
+        },
+      },
+    });
+
+    expect(res.errors?.length).toBeGreaterThan(0);
+    expect(res.errors![0]).toHaveProperty('message');
     expect(res).toMatchSnapshot();
   })
 });
