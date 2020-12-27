2052e8dc4518c323979a5f0a41e3db66f2c1e18f
hay-bams <purposenigeria@gmail.com>
2020-12-27 21:10:17 +0100

    add more sidebar link

--- client/src/lib/components/Sidebar/index.tsx
+++ client/src/lib/components/Sidebar/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import {
   UserOutlined,
+  UserAddOutlined,
   BookOutlined,
   FolderAddOutlined,
 } from '@ant-design/icons';
@@ -35,8 +36,14 @@ export const Sidebar = () => {
         <Menu.Item key="1" icon={<UserOutlined />}>
           <Link to="/students">All Students</Link>
         </Menu.Item>
-        <Menu.Item key="2" icon={<UserOutlined />}>
-          <Link to="/new-student">Add New Students</Link>
+        <Menu.Item key="3" icon={<FolderAddOutlined />}>
+          <Link to="/courses">All Courses</Link>
+        </Menu.Item>
+        <Menu.Item key="2" icon={<UserAddOutlined />} >
+          <Link to="/new-student">Add New Student</Link>
+        </Menu.Item>
+        <Menu.Item key="3" icon={<BookOutlined />}>
+          <Link to="/new-course">Add New Course</Link>
         </Menu.Item>
       </Menu>
     </Sider>
