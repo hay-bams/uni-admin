df77c2f62c204352bb5c320068d9660347a1ed0a
hay-bams <purposenigeria@gmail.com>
2020-12-29 14:51:18 +0100

    update authentication

--- server/src/utils/authenticate.ts
+++ server/src/utils/authenticate.ts
@@ -12,20 +12,20 @@ export const Authenticated =  <T=any, K=any> (next: any) => async (
   const token = req.get("X-CSRF-TOKEN");
 
   const id_cookie = req.signedCookies.admin;
-  // const decoded = jwt.verify(`${token}`, `${process.env.SECRET}`) as { admin: string }
+  const decoded = jwt.verify(`${token}`, `${process.env.SECRET}`) as { admin: string }
 
-  // if(id_cookie !== decoded.admin) {
-  //   throw new Error('This may be a CSRF attack, please sign in')
-  // }
+  if(id_cookie !== decoded.admin) {
+    throw new Error('This may be a CSRF attack, please sign in')
+  }
 
-  // const admin = await db.users.findOne({
-  //   _id: new ObjectID(id_cookie),
-  // });
+  const admin = await db.users.findOne({
+    _id: new ObjectID(id_cookie),
+  });
 
-  // if (!admin) {
-  //   res.clearCookie('admin');
-  //   throw new Error('User not authenticated, please sign in first');
-  // }
+  if (!admin) {
+    res.clearCookie('admin');
+    throw new Error('User not authenticated, please sign in first');
+  }
 
   return next(root, args, ctx);
 };
